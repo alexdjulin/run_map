d20fe8f7955a3116f259efb1f4640a40ab021e21
Alexandre D-Julin <53292656+adjulin@users.noreply.github.com>
2021-03-21 00:11:57 +0100

    added all races and fixed icons

--- run_map.py
+++ run_map.py
@@ -9,21 +9,17 @@ events_cst = "events.csv"
 
 # define race colors based on distance and store them in a dictionnary
 race_color_dict = {
-'5': "rgb(155, 255, 0)",
-'10': "rgb(55, 175, 255)",
-'21.1': "rgb(135, 55, 255)",
-'42.2': "rgb(192,0,0)",
-'ultra': "rgb(0,0,0)",
-'misc': "rgb(255,213,0)"
+'5.0': "beige",
+'10.0': "blue",
+'21.1': "green",
+'42.2': "red",
+'ultra': "black",
+'misc': "lightgray"
 }
 
 # load html popup contents
 with open(popup_contents_html) as f:
     html_popup = f.read()
-
-# create map centered on West Europe
-# IMPROVEMENT: Calculate center of map based on race locations
-run_map = folium.Map(location=[49.799891255201715, 10.580801170359733], tiles="OpenStreetMap", zoom_start=5)
 
 # get data from csv file
 data = pandas.read_csv(events_cst)
@@ -37,32 +33,38 @@ dist_list = list(data["Distance"])
 time_list = list(data["Time"])
 notes_list = list(data["Notes"])
 link_list = list(data["Link"])
+post_list = list(data["Post"])
 pic_list = list(data["Picture"])
+
+# calculate center of map based on race locations
+start_lat = (min(lat_list)+max(lat_list))/2
+start_lon = (min(lon_list)+max(lon_list))/2
+run_map = folium.Map(location=[start_lat, start_lon], tiles="openstreetmap", zoom_start=6)
 
 # create feature group
 fg = folium.FeatureGroup(name="My Map")
 
-# points_coordinates.append(([52.51555779949443, 13.367028952828266], "Berlin Marathon"))
-# points_coordinates.append(([53.01391155869189, 13.997051337762928], "Schorfheide marathon"))
-# points_coordinates.append(([48.85896612853125, 2.2935252477178847], "Eco-Trail de Paris"))
-# points_coordinates.append(([51.79943900260884, 10.615524864337875], "Harz Broken marathon"))
-# points_coordinates.append(([48.232030367113396, 16.41620554169843], "Vienna marathon"))
-# points_coordinates.append(([54.31626411025583, 13.126867687637457], "Rugen marathon"))
-
 # add markers based on csv file data
-for date, race, loc, lt, ln, typ, dist, time, notes, link, pic in zip(date_list, race_list, loc_list, lat_list, lon_list, type_list, dist_list, time_list, notes_list, link_list, pic_list):
+for date, race, loc, lt, ln, typ, dist, time, notes, link, post, pic in zip(date_list, race_list, loc_list, lat_list, lon_list, type_list, dist_list, time_list, notes_list, link_list, post_list, pic_list):
     
     # define race_color icon and title based on the distance
     str_dist = str(dist)
+    race_color = ""
+    
     if str_dist in race_color_dict.keys():
         race_color = race_color_dict[str_dist]
     elif dist > 42.2:
         race_color = race_color_dict['ultra']
     else:
         race_color = race_color_dict['misc']
-    
-    # create the iFrame
-    iframe = folium.IFrame(html=html_popup.format(race=race, date=date, loc=loc, typ=typ, dist=str_dist+' km', time=time, notes=notes, link=link, pic=pic, race_clr=race_color), width=450, height=300)
+
+    # delete the blog post line if link not in csv file
+    if not str(post).lower().startswith('http'):
+        html_contents = html_popup.replace("Read Blog Post", "")
+    else:
+        html_contents = html_popup
+
+    iframe = folium.IFrame(html=html_contents.format(race=race, date=date, loc=loc, typ=typ, dist=str_dist+' km', time=time, notes=notes, link=link, post=post, pic=pic, race_clr=race_color), width=500, height=350)
     
     # add marker to feature group
     fg.add_child(folium.Marker(location=[lt, ln], tooltip=race, popup=folium.Popup(iframe), icon = folium.Icon(color = race_color)))
